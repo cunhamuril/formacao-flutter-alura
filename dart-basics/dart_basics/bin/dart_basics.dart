821f71c6836ca046ebe46919ca33a7889f46d82e
Murilo Cunha <murilo.sant@hotmail.com>
2022-07-01 17:03:51 -0300

    feat(dart_basics): variables

--- dart-basics/dart_basics/bin/dart_basics.dart
+++ dart-basics/dart_basics/bin/dart_basics.dart
@@ -1,5 +1,41 @@
-import 'package:dart_basics/dart_basics.dart' as dart_basics;
+void main() {
+  /**
+   * Numbers
+   */
+  // int age = 27;
+  int age = 0x0001B; // também aceita hexadecimal
+  double tall = 1.79;
 
-void main(List<String> arguments) {
-  print('Hello world: ${dart_basics.calculate(2, 2)}!');
+  // Suporta formato exponencial
+  double exponential = 780e6; // 780 * 10 ^ 6
+
+  print(age); // 27
+  print(tall); // 1.79
+  print(exponential); // 780000000
+
+  /**
+   * Booleans
+   */
+  bool isGeek = true;
+  bool isAgeEqualtall = age == tall;
+
+  print(isGeek); // true
+  print(isAgeEqualtall); // false
+
+  /**
+   * Strings
+   */
+  String name = 'Murilo';
+  String nickname = 'Turnymann';
+
+  print(name);
+  print(nickname);
+
+  String phrase = 'I am $nickname\n'
+      'but my name is $name. \n'
+      'Am I geek? $isGeek. \n'
+      'I have $tall feet tall. \n'
+      'And I have $age years old.';
+
+  print(phrase);
 }
