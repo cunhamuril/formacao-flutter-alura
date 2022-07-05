d125adc38cd8db3155d4d3470204bb2c05054640
Murilo Cunha <murilo.sant@hotmail.com>
2022-07-05 11:29:42 -0300

    refactor(bytebank): extracting widgets in classes

--- bytebank/lib/main.dart
+++ bytebank/lib/main.dart
@@ -3,24 +3,7 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
     home: Scaffold(
-      body: Column(
-        children: const <Widget>[
-          Card(
-            child: ListTile(
-              leading: Icon(Icons.monetization_on),
-              title: Text('100.0'),
-              subtitle: Text('1000'),
-            ),
-          ),
-          Card(
-            child: ListTile(
-              leading: Icon(Icons.monetization_on),
-              title: Text('300.0'),
-              subtitle: Text('1000'),
-            ),
-          ),
-        ],
-      ),
+      body: const TransfersList(),
       appBar: AppBar(
         title: const Text('Transferências'),
       ),
@@ -30,4 +13,43 @@ void main() {
       ),
     ),
   ));
+}
+
+class TransfersList extends StatelessWidget {
+  const TransfersList({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        TransferItem(transfer: Transfer(100.0, 1000)),
+        TransferItem(transfer: Transfer(200.0, 2000)),
+        TransferItem(transfer: Transfer(300.0, 3000)),
+      ],
+    );
+  }
+}
+
+class TransferItem extends StatelessWidget {
+  final Transfer transfer;
+
+  const TransferItem({super.key, required this.transfer});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        leading: const Icon(Icons.monetization_on),
+        title: Text(transfer.value.toString()),
+        subtitle: Text(transfer.account.toString()),
+      ),
+    );
+  }
+}
+
+class Transfer {
+  final double value;
+  final int account;
+
+  Transfer(this.value, this.account);
 }
