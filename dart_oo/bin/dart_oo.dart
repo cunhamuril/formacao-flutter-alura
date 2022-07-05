5196a7784b0327b6bf890f5fe6327a472a418cd9
Murilo Cunha <murilo.sant@hotmail.com>
2022-07-05 17:01:26 -0300

    feat(dart_oo): classes & methods

--- dart_oo/bin/dart_oo.dart
+++ dart_oo/bin/dart_oo.dart
@@ -1,31 +1,58 @@
-import 'dart:ffi';
-
 void main() {
-  String name = "Laranja";
-  double weight = 100.2;
-  String color = "Verde e Amarela";
-  String flavor = "Doce e cítrica";
-  int daysSinceHarvest = 30;
+  // String name = "Laranja";
+  // double weight = 100.2;
+  // String color = "Verde e Amarela";
+  // String flavor = "Doce e cítrica";
+  // int daysSinceHarvest = 30;
 
-  showMature(
-    name,
-    daysSinceHarvest,
-    flavor: flavor,
-    color: color,
-    weight: weight,
+  Fruit orange = Fruit(
+    "Laranja",
+    100.2,
+    "Verde e Amarela",
+    "Doce e cítrica",
+    30,
   );
 
-  showMature(
+  Fruit grape = Fruit(
     "Uva",
-    20,
-    color: 'Roxa',
-    weight: 40,
+    40,
+    "Roxa",
+    "Doce",
+    5,
   );
 
-  print(howManyDaysToMature(20));
+  Fruit banana = Fruit.nominated(
+    name: 'Banana',
+    weight: 84.95,
+    color: 'Amarela',
+    daysSinceHarvest: 10,
+    flavor: 'Doce',
+  );
+
+  // showMature(
+  //   orange.name,
+  //   orange.daysSinceHarvest,
+  //   flavor: orange.flavor,
+  //   color: orange.color,
+  //   weight: orange.weight,
+  // );
+
+  // showMature(
+  //   grape.name,
+  //   grape.daysSinceHarvest,
+  //   color: grape.color,
+  //   weight: grape.weight,
+  // );
+
+  // print(howManyDaysToMature(20));
+
+  banana.checkMature(30);
+  grape.checkMature(40);
+
+  print(orange.checkMature(10));
 }
 
-bool isMature(int daysSinceHarvest) => daysSinceHarvest >= 30;
+// bool isMature(int daysSinceHarvest) => daysSinceHarvest >= 30;
 
 // NULL SAFETY => Garantia dentro de uma linguagem de programação orientada a objetos que nenhuma referência de objeto terá valores nulos.
 
@@ -59,4 +86,54 @@ int howManyDaysToMature(int days) {
   int daysMissing = daysToMature - days;
 
   return daysMissing;
+}
+
+class Fruit {
+  String name;
+  double weight;
+  String color;
+  String flavor;
+  int daysSinceHarvest;
+  bool? isMature;
+
+  Fruit(
+    this.name,
+    this.weight,
+    this.color,
+    this.flavor,
+    this.daysSinceHarvest, {
+    this.isMature,
+  });
+
+  // /// Uma classe pode ter mais de um tipo de construtor
+
+  // Exemplos de diferentes construtores
+  Fruit.nominated({
+    required this.name,
+    required this.color,
+    required this.weight,
+    required this.daysSinceHarvest,
+    required this.flavor,
+    this.isMature,
+  });
+
+  Fruit.lowerCase(
+    this.name,
+    this.color,
+    this.daysSinceHarvest,
+    this.flavor,
+    this.isMature,
+    this.weight,
+  ) {
+    name = name.toLowerCase();
+    color = color.toLowerCase();
+  }
+  // ///
+
+  checkMature(int daysToMature) {
+    isMature = daysSinceHarvest >= daysToMature;
+
+    print(
+        "A sua $name foi colhida a $daysSinceHarvest dias, e precisa de $daysToMature para poder comer. Ela está madura? $isMature");
+  }
 }
