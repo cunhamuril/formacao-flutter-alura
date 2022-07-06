dd7c106b0438d51a4b1ed667130aefe030ded114
Murilo Cunha <murilo.sant@hotmail.com>
2022-07-06 11:50:08 -0300

    feat(bytebank): done transfer form layout

--- bytebank/lib/main.dart
+++ bytebank/lib/main.dart
@@ -1,18 +1,21 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: Scaffold(
-      body: const TransfersList(),
-      appBar: AppBar(
-        title: const Text('Transferências'),
+  runApp(const ByteBankApp());
+}
+
+class ByteBankApp extends StatelessWidget {
+  const ByteBankApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(
+        // body: TransfersList(),
+        body: TransferForm(),
       ),
-      floatingActionButton: const FloatingActionButton(
-        onPressed: null,
-        child: Icon(Icons.add),
-      ),
-    ),
-  ));
+    );
+  }
 }
 
 class TransfersList extends StatelessWidget {
@@ -20,12 +23,21 @@ class TransfersList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        TransferItem(transfer: Transfer(100.0, 1000)),
-        TransferItem(transfer: Transfer(200.0, 2000)),
-        TransferItem(transfer: Transfer(300.0, 3000)),
-      ],
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Transferências'),
+      ),
+      body: Column(
+        children: <Widget>[
+          TransferItem(transfer: Transfer(100.0, 1000)),
+          TransferItem(transfer: Transfer(200.0, 2000)),
+          TransferItem(transfer: Transfer(300.0, 3000)),
+        ],
+      ),
+      floatingActionButton: const FloatingActionButton(
+        onPressed: null,
+        child: Icon(Icons.add),
+      ),
     );
   }
 }
@@ -52,4 +64,54 @@ class Transfer {
   final int account;
 
   Transfer(this.value, this.account);
+}
+
+class TransferForm extends StatelessWidget {
+  const TransferForm({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Criando transferência'),
+      ),
+      body: Column(
+        children: const <Widget>[
+          Padding(
+            // padding: EdgeInsets.fromLTRB(16, 8, 16, 8),
+            padding: EdgeInsets.all(16),
+            child: TextField(
+              style: TextStyle(
+                fontSize: 16,
+              ),
+              decoration: InputDecoration(
+                labelText: 'Número da conta',
+                hintText: '0000',
+              ),
+              keyboardType: TextInputType.number,
+            ),
+          ),
+          Padding(
+            // padding: EdgeInsets.fromLTRB(16, 8, 16, 8),
+            padding: EdgeInsets.all(16),
+            child: TextField(
+              style: TextStyle(
+                fontSize: 16,
+              ),
+              decoration: InputDecoration(
+                icon: Icon(Icons.monetization_on),
+                labelText: 'Valor',
+                hintText: '0.00',
+              ),
+              keyboardType: TextInputType.number,
+            ),
+          ),
+          ElevatedButton(
+            onPressed: null,
+            child: Text('Confirmar'),
+          )
+        ],
+      ),
+    );
+  }
 }
