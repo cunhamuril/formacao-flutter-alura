8499e9b1656623cb8da2083f64343f7f5e36ac10
Murilo Cunha <murilo.sant@hotmail.com>
2022-07-11 13:29:23 -0300

    refactor(bytebank_2:app_database): replacing then to async await in functions

--- bytebank_2/lib/database/app_database.dart
+++ bytebank_2/lib/database/app_database.dart
@@ -3,53 +3,95 @@ import 'package:sqflite/sqflite.dart';
 
 import 'package:bytebank_2/models/contact.dart';
 
-Future<Database> createDatabase() {
-  return getDatabasesPath().then((dbPath) {
-    final String path = join(dbPath, 'bytebank.db');
+Future<Database> getDatabase() async {
+  final String path = join(await getDatabasesPath(), 'bytebank.db');
 
-    return openDatabase(
-      path,
-      onCreate: (db, version) {
-        db.execute(
-          'CREATE TABLE contacts('
-          'id INTEGER PRIMARY KEY,'
-          'name TEXT,'
-          'account_number INTEGER)',
-        );
-      },
-      version: 1,
-      // onDowngrade: onDatabaseDowngradeDelete, // Limpar o DB
+  return openDatabase(
+    path,
+    onCreate: (db, version) {
+      db.execute(
+        'CREATE TABLE contacts('
+        'id INTEGER PRIMARY KEY,'
+        'name TEXT,'
+        'account_number INTEGER)',
+      );
+    },
+    version: 1,
+    // onDowngrade: onDatabaseDowngradeDelete, // Limpar o DB
+  );
+
+  // return getDatabasesPath().then((dbPath) {
+  //   final String path = join(dbPath, 'bytebank.db');
+
+  //   return openDatabase(
+  //     path,
+  //     onCreate: (db, version) {
+  //       db.execute(
+  //         'CREATE TABLE contacts('
+  //         'id INTEGER PRIMARY KEY,'
+  //         'name TEXT,'
+  //         'account_number INTEGER)',
+  //       );
+  //     },
+  //     version: 1,
+  //     // onDowngrade: onDatabaseDowngradeDelete, // Limpar o DB
+  //   );
+  // });
+}
+
+Future<int> save(Contact contact) async {
+  final Database db = await getDatabase();
+  final Map<String, dynamic> contactMap = {};
+
+  contactMap['name'] = contact.name;
+  contactMap['account_number'] = contact.accountNumber;
+
+  return db.insert('contacts', contactMap);
+
+  // return getDatabase().then((db) {
+  //   final Map<String, dynamic> contactMap = {};
+
+  //   contactMap['name'] = contact.name;
+  //   contactMap['account_number'] = contact.accountNumber;
+
+  //   return db.insert('contacts', contactMap);
+  // });
+}
+
+Future<List<Contact>> findAll() async {
+  final Database db = await getDatabase();
+
+  final List<Map<String, dynamic>> query = await db.query('contacts');
+
+  final List<Contact> contacts = [];
+
+  for (Map<String, dynamic> row in query) {
+    final Contact contact = Contact(
+      row['id'],
+      row['name'],
+      row['account_number'],
     );
-  });
-}
 
-Future<int> save(Contact contact) {
-  return createDatabase().then((db) {
-    final Map<String, dynamic> contactMap = {};
+    contacts.add(contact);
+  }
 
-    contactMap['name'] = contact.name;
-    contactMap['account_number'] = contact.accountNumber;
+  return contacts;
 
-    return db.insert('contacts', contactMap);
-  });
-}
+  // return getDatabase().then((db) {
+  //   return db.query('contacts').then((maps) {
+  //     final List<Contact> contacts = [];
 
-Future<List<Contact>> findAll() {
-  return createDatabase().then((db) {
-    return db.query('contacts').then((maps) {
-      final List<Contact> contacts = [];
+  //     for (Map<String, dynamic> map in maps) {
+  //       final Contact contact = Contact(
+  //         map['id'],
+  //         map['name'],
+  //         map['account_number'],
+  //       );
 
-      for (Map<String, dynamic> map in maps) {
-        final Contact contact = Contact(
-          map['id'],
-          map['name'],
-          map['account_number'],
-        );
+  //       contacts.add(contact);
+  //     }
 
-        contacts.add(contact);
-      }
-
-      return contacts;
-    });
-  });
+  //     return contacts;
+  //   });
+  // });
 }
