dfcac52a114965afda16fbbab2c1859cce6fdd9d
Murilo Cunha <murilo.sant@hotmail.com>
2022-07-13 12:03:08 -0300

    feat(bytebank_2): web api initial setup

---
+++ bytebank_2/lib/http/client.dart
@@ -0,0 +1,58 @@
+import 'dart:convert';
+
+import 'package:bytebank_2/models/contact.dart';
+import 'package:bytebank_2/models/transaction.dart';
+import 'package:http/http.dart';
+import 'package:http_interceptor/http/http.dart';
+import 'package:http_interceptor/models/request_data.dart';
+import 'package:http_interceptor/models/response_data.dart';
+
+const baseUrl = 'http://192.168.0.109:8080';
+
+class LoggingInterceptor implements InterceptorContract {
+  @override
+  Future<RequestData> interceptRequest({required RequestData data}) async {
+    print('REQUEST');
+    print('URL: ${data.baseUrl}');
+    print('HEADERS: ${data.headers}');
+    print('BODY: ${data.body}');
+    return data;
+  }
+
+  @override
+  Future<ResponseData> interceptResponse({required ResponseData data}) async {
+    print('RESPONSE');
+    print('STATUS CODE: ${data.statusCode}');
+    print('HEADERS: ${data.headers}');
+    print('BODY: ${data.body}');
+    return data;
+  }
+}
+
+Future<List<Transaction>> findAll() async {
+  final Client client = InterceptedClient.build(
+    interceptors: [LoggingInterceptor()],
+  );
+
+  final Response response = await client.get(
+    Uri.parse('$baseUrl/transactions'),
+  );
+
+  final List<dynamic> transactionsDecodedJson = jsonDecode(response.body);
+  final List<Transaction> transactions = [];
+
+  for (Map<String, dynamic> transaction in transactionsDecodedJson) {
+    transactions.add(
+      Transaction(
+        transaction['value'],
+        Contact(
+          0,
+          transaction['contact']['name'],
+          transaction['contact']['accountNumber'],
+        ),
+      ),
+    );
+  }
+
+  return transactions;
+}
--- bytebank_2/lib/main.dart
+++ bytebank_2/lib/main.dart
@@ -1,9 +1,11 @@
+import 'package:bytebank_2/http/client.dart';
 import 'package:flutter/material.dart';
 
 import 'package:bytebank_2/screens/dashboard.dart';
 
 void main() {
   runApp(const BytebankApp());
+  findAll().then((transactions) => print('new transactions $transactions'));
 }
 
 class BytebankApp extends StatelessWidget {
